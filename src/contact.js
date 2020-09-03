5ed88300ece656163bfb79fb84ba880fbb5571b2
tomigm <tgmoron@gmail.com>
2020-09-03 17:43:53 -0300

    modified contact.js

--- src/contact.js
+++ src/contact.js
@@ -3,9 +3,7 @@ contact.classList.add("contact");
 const map = document.createElement("iframe");
 
 map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.987531320238!2d-58.381619860095526!3d-34.60447680158088!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4aa9f0a6da5edb%3A0x11bead4e234e558b!2sObelisco!5e0!3m2!1ses-419!2sar!4v1599000302652!5m2!1ses-419!2sar"
-map.width="600";
-map.height="450";
-map.frameborder="0";
+
 map.style="border:0;";
 map.allowfullscreen="";
 map["aria-hidden"]="false";
